6f602a58b6f2fa00164b176e427beb3a12283404
shuheiintokyo <92214102+shuheiintokyo@users.noreply.github.com>
2025-09-30 11:57:29 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  )
 }
