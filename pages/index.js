cbfb626f7bc222ce823a809d5558be624393dcee
Kinugasa Hirata <kinugasa.hirata@gmail.com>
2025-10-16 17:43:09 +0900

    add supabase

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { supabase } from '../lib/supabase';
 import { QrCode, Plus, MessageSquare, LogOut, Edit, Download, Trash2, Bell, HelpCircle, X } from 'lucide-react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
