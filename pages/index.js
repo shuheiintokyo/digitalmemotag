20879f8ed30280b3fa4c4216d8462457f12c1761
Kinugasa Hirata <kinugasa.hirata@gmail.com>
2025-10-16 17:41:13 +0900

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { supabase } from '../lib/supabase';
 import { QrCode, Plus, MessageSquare, LogOut, Edit, Download, Trash2, Bell, HelpCircle, X } from 'lucide-react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
